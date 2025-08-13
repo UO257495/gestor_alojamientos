c00b8841d2d2968ff3695cf3a3f1986f6aa04d44
Nayara Sánchez <nayarasanchz@gmail.com>
2025-08-13 14:10:10 +0200

    Estructura base del proyecto generada con Spring Initializr

---
+++ src/main/java/com/nayarasanchez/gestor_alojamientos/GestorAlojamientosApplication.java
@@ -0,0 +1,13 @@
+package com.nayarasanchez.gestor_alojamientos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorAlojamientosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorAlojamientosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nayarasanchez/gestor_alojamientos/GestorAlojamientosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayarasanchez.gestor_alojamientos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorAlojamientosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
