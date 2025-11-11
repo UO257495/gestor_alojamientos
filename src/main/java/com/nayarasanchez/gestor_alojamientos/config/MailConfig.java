29415517284a19dba8e39b4ca1d74111527a1694
Nayara Sánchez <nayarasanchz@gmail.com>
2025-11-11 18:47:28 +0100

    Nuevos fixes de seguridad para email

---
+++ src/main/java/com/nayarasanchez/gestor_alojamientos/config/MailConfig.java
@@ -0,0 +1,35 @@
+package com.nayarasanchez.gestor_alojamientos.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        Dotenv dotenv = Dotenv.load();
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(dotenv.get("EMAIL_USER"));
+        mailSender.setPassword(dotenv.get("EMAIL_PASS"));
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.starttls.required", "true");
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
